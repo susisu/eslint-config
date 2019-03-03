4128be69b3c7ac7ce9d3d7bbc331abdbb8735bd3
Susisu <susisu2413@yahoo.co.jp>
2019-03-04 00:57:47 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  "extends": ["./lib/base.js"],
+  "parserOptions": {
+    "ecmaVersion": 2018,
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+``` shell
+npm i -D eslint @susisu/eslint-config
+yarn add -D eslint @susisu/eslint-config
+```
