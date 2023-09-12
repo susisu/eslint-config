ecdbb503d7ee63a42edb3534476b13beaccce31b
Susisu <susisu2413@yahoo.co.jp>
2023-09-13 04:39:04 +0900

    feat: change main to flat config

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const js = require("./config/js");
+const ts = require("./config/ts");
+const tsTypeChecked = require("./config/ts-type-checked");
+
+module.exports = {
+  js,
+  ts,
+  tsTypeChecked,
+};
