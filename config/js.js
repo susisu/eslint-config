edee061c6d30f60d8cc90878a02f630479b30315
Susisu <susisu2413@yahoo.co.jp>
2023-12-02 19:21:45 +0900

    chore: install stylistic plugin

--- config/js.js
+++ config/js.js
@@ -1,12 +1,21 @@
 "use strict";
 
+const styliticPlugin = require("@stylistic/eslint-plugin");
+
 const jsRules = require("../lib/rules/js");
+const stylisticRules = require("../lib/rules/stylistic");
 
 module.exports = () => {
   return {
     linterOptions: {
       reportUnusedDisableDirectives: true,
     },
-    rules: jsRules.rules,
+    plugins: {
+      "@stylistic": styliticPlugin,
+    },
+    rules: {
+      ...jsRules.rules,
+      ...stylisticRules.rules,
+    },
   };
 };
