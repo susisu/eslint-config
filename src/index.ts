802cb5b83f4c8df1cbaa8046532dcbee6750a977
Susisu <susisu2413@yahoo.co.jp>
2023-12-16 21:47:06 +0900

    style: ignore naming convention

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from "./config";
 import { map } from "./utils";
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 import * as __internal__rules from "./rules";
 
 export { config, map, __internal__rules };
