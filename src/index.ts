b8c3c710617fe9b885f068a61435c192a61c6467
Susisu <susisu2413@yahoo.co.jp>
2024-02-11 15:27:07 +0900

    feat: export ConfigOptions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import type { ConfigOptions } from "./config";
 import { config } from "./config";
 import { map } from "./utils";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import * as __internal__rules from "./rules";
 
-export { config, map, __internal__rules };
+export { ConfigOptions, config, map, __internal__rules };
 export default config;
