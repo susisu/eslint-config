779efdeeff9e84a054fe44c69b7895ae07698801
Susisu <susisu2413@yahoo.co.jp>
2024-07-08 01:42:33 +0900

    refactor: add a new rule

--- src/rules/ts.ts
+++ src/rules/ts.ts
@@ -163,6 +163,7 @@ const suggestions: Rules = {
   "@typescript-eslint/no-dynamic-delete": off,
   "@typescript-eslint/no-empty-function": off,
   "@typescript-eslint/no-empty-interface": off,
+  "@typescript-eslint/no-empty-object-type": off,
   "@typescript-eslint/no-explicit-any": warn,
   "@typescript-eslint/no-extraneous-class": [
     error,
