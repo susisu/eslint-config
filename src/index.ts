567ea79be9694d143909ffd0faba4ea62e846a7a
Susisu <susisu2413@yahoo.co.jp>
2024-10-26 11:37:30 +0900

    fix: use consistent type imports/exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import type { ConfigOptions } from "./config";
 import { config } from "./config";
-import { ConfigWithExtends, map } from "./utils";
+import type { ConfigWithExtends } from "./utils";
+import { map } from "./utils";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import * as __internal__rules from "./rules";
 
-export { ConfigOptions, ConfigWithExtends, config, map, __internal__rules };
+export type { ConfigOptions, ConfigWithExtends };
+export { config, map, __internal__rules };
 export default config;
