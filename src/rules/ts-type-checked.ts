ec47202dcdcd40c112cf31a7eaa0a9a6d59924e1
Susisu <susisu2413@yahoo.co.jp>
2024-11-23 18:21:04 +0900

    chore: add no-unsafe-type-assertion

--- src/rules/ts-type-checked.ts
+++ src/rules/ts-type-checked.ts
@@ -35,6 +35,8 @@ const possibleProblems: Rules = {
   "@typescript-eslint/no-unsafe-call": warn,
   "@typescript-eslint/no-unsafe-member-access": warn,
   "@typescript-eslint/no-unsafe-return": warn,
+  // any kind of type assertions are prohibited by @susisu/safe-typescript/no-type-assertion
+  "@typescript-eslint/no-unsafe-type-assertion": off,
   "@typescript-eslint/no-unsafe-unary-minus": error,
   "@typescript-eslint/only-throw-error": [
     error,
