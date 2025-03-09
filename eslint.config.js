e3f670ba6dc6641208a0305279bb5c3f317bd181
Susisu <susisu2413@yahoo.co.jp>
2025-03-09 18:57:19 +0900

    chore(deps): upgrade eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -5,14 +5,12 @@ export default config(
   {
     tsconfigRootDir: import.meta.dirname,
   },
-  [
-    {
-      languageOptions: {
-        globals: {
-          ...globals.es2023,
-          ...globals.node,
-        },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es2023,
+        ...globals.node,
       },
     },
-  ],
+  },
 );
