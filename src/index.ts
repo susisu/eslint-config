8b0ddb6ce9cb6594606197e4cfcfe1f5073a5ed4
Susisu <susisu2413@yahoo.co.jp>
2025-03-09 18:09:05 +0900

    feat: remove ConfigWithExtends type and expand function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import type { ConfigOptions } from "./config/index.js";
 import { config } from "./config/index.js";
-import type { ConfigWithExtends } from "./utils/index.js";
 
-export type { ConfigOptions, ConfigWithExtends };
+export type { ConfigOptions };
 export { config };
 export default config;
