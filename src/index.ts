8b20fc8ab584a212d8eab54d9e80ab8f1dfcc386
Susisu <susisu2413@yahoo.co.jp>
2025-03-09 17:57:43 +0900

    refactor: remove __internal__rules

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import { config } from "./config/index.js";
 import type { ConfigWithExtends } from "./utils/index.js";
 import { map } from "./utils/index.js";
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-import * as __internal__rules from "./rules/index.js";
-
 export type { ConfigOptions, ConfigWithExtends };
-export { config, map, __internal__rules };
+export { config, map };
 export default config;
